0571793f1d142ff12afb92715b247f048369407e
hwh40508176 <maru15995187@gmail.com>
2020-05-29 08:21:14 +0800

    完成頭盔版 VR 場景

---
+++ 0508525_VRVIVE/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("躲避球數量")]
+    public Text textBallCount;
+    [Header("分數")]
+    public Text textScore;
+
+    int ballCount = 5;
+    int score;
+
+    public void UseBall(GameObject ball)
+    {
+        Destroy(ball.GetComponent<Throwable>());
+        Destroy(ball.GetComponent<Interactable>());
+
+        ballCount--;
+
+        textBallCount.text = "躲避球數量：" + ballCount + " / 5";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score += 2;
+        textScore.text = "分數：" + score;
+    }
+
+}
+
